ebeae63d861a1484068b706cc63cb76124cc839a
George Tokmaji <tokmajigeorge@gmail.com>
2021-12-02 16:42:01 +0100

    Mac: Use GCC 11

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "CC=gcc-11" >> $GITHUB_ENV
 echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -C $PWD/config.txt" >> $GITHUB_ENV
 LIBS=("\"-framework "{CoreAudio,CoreVideo,AudioToolbox,AudioUnit,ForceFeedback,IOKit,Cocoa,Foundation,AVFoundation,Carbon}\")
 echo "set(EXTRA_LINK_LIBS ${LIBS[@]} CACHE STRING \"Extra libs needed for static linking\")" > config.txt
